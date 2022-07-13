88c30ce24ee05800197e2156c7e8f808600e8ee4
seongjin77 <kssj9056@gmail.com>
2022-07-13 19:36:30 +0900

    first commit login

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './LoginPage';
+
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
